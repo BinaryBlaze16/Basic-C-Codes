3405341ef96a318dbaa1950cf14297328d90ee7b
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-16 23:10:26 +0530

    add Fibonacci series checker program

---
+++ 93.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, c;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n == 0 || n == 1) {
+        printf("%d is in the Fibonacci series.\n", n);
+        return 0;
+    }
+
+    c = a + b;
+    while (c <= n) {
+        if (c == n) {
+            printf("%d is in the Fibonacci series.\n", n);
+            return 0;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+
+    printf("%d is NOT in the Fibonacci series.\n", n);
+    return 0;
+}
