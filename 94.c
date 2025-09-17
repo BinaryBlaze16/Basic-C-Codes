abd0e75e88e532d541835a1306cbbe7854344bbf
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-17 22:15:36 +0530

    remove unnecessary blank lines in HCF calculation program

--- 94.c
+++ 94.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 int main() {
     int a, b, temp, hcf;
-
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     int x = a, y = b;
@@ -12,8 +10,6 @@ int main() {
         x = temp;
     }
     hcf = x;
-
     printf("HCF of %d and %d is: %d\n", a, b, hcf);
-
     return 0;
 }
