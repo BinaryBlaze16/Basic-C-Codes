7f1a7c959c42a0d78b5b529aae87f5985005c1a5
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-18 23:47:32 +0530

    refactor: improve formatting and structure of GCD program

--- 95.c
+++ 95.c
@@ -1,4 +1,23 @@
-#include<stdio.h>
-int main() {
+#include <stdio.h>
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
 
-}    
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (gcd(num1, num2) == 1) {
+        printf("%d and %d are Co-Prime.\n", num1, num2);
+    } else {
+        printf("%d and %d are NOT Co-Prime.\n", num1, num2);
+    }
+
+    return 0;
+}
