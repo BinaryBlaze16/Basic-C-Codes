7ceab7d7052c9aa3b3bd214d7590c64a8e009f81
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-19 22:13:02 +0530

    fix: correct prime number detection logic in main function

--- 96.c
+++ 96.c
@@ -1,4 +1,18 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+
+int main() {
+    int i, j, isPrime;
+    for (i = 2; i <= 100; i++) {
+        isPrime = 1; // Assume number is prime
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
 }
