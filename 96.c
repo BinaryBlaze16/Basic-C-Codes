0663e70f9c1fabeeba289a29450ceef4e6ce95a0
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-19 22:13:19 +0530

    fix: remove redundant comment in prime number detection logic

--- 96.c
+++ 96.c
@@ -3,7 +3,7 @@
 int main() {
     int i, j, isPrime;
     for (i = 2; i <= 100; i++) {
-        isPrime = 1; // Assume number is prime
+        isPrime = 1; 
         for (j = 2; j * j <= i; j++) {
             if (i % j == 0) {
                 isPrime = 0;
