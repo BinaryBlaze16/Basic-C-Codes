a3c80b2a33fb14c724db15fb6e0778717690d5e6
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-21 19:23:39 +0530

    refactor: restructure prime number program with function for prime checking

--- 98.c
+++ 98.c
@@ -1,5 +1,26 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, next;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    next = n + 1; // start checking from next number
+    while (!isPrime(next)) {
+        next++;
+    }
+
+    printf("Next prime number is: %d\n", next);
     return 0;
 }
