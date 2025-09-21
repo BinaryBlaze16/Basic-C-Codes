d85647af73abf3504a4c0d2deaa32d711d7885a8
BinaryBlaze16 <156859572+BinaryBlaze16@users.noreply.github.com>
2025-09-21 19:24:03 +0530

    fix: remove redundant comment and clean up whitespace in prime number program

--- 98.c
+++ 98.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-// Function to check if a number is prime
 bool isPrime(int num) {
     if (num <= 1) return false;
     for (int i = 2; i * i <= num; i++) {
@@ -16,7 +14,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    next = n + 1; // start checking from next number
+    next = n + 1;
     while (!isPrime(next)) {
         next++;
     }
